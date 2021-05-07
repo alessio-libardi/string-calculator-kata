271c1d4b9db94d1a8a8c9ac86b0930f7035959fb
Alessio Libardi <alessio@wonderflow.co>
2021-05-07 11:16:08 +0200

    refactor: Step 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+const DEFAULT_DELIMITER = ','
+const NEW_LINES_DELIMITER = /[\n]+/
+
 function add(numbers) {
   const isEmpty = numbers === ''
   if (isEmpty) {
     return numbers
   }
 
-  const integers = numbers.split(/[(\r\n|\r|\n),]+/).map((n) => parseInt(n))
+  numbers = numbers.replace(NEW_LINES_DELIMITER, DEFAULT_DELIMITER)
 
+  const matches = numbers.split(DEFAULT_DELIMITER)
+  const integers = matches.map((n) => parseInt(n))
   return integers.reduce((acc, curr) => acc + curr, 0)
 }
 
