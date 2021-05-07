6a5d091f127cf8a2b373d77b29e3f14eee9b8b3c
Alessio Libardi <alessio@wonderflow.co>
2021-05-07 09:51:06 +0200

    test: Defined Step 1 tests

---
+++ src/index.spec.js
@@ -0,0 +1,11 @@
+import { add } from '.'
+
+describe('String calculator function', function () {
+  describe('Step 1: the simplest thing', function () {
+    it.todo('accept an empty string')
+
+    it.todo('accept a string with one number')
+
+    it.todo('accept a string with two numbers')
+  })
+})
