bedbac52a2fbf73ada72861e8748a25f994a0cab
Dev Wonderflow <58233289+alessio-libardi@users.noreply.github.com>
2021-11-28 20:27:26 +0100

    feat: support an unknown amount of numbers

--- src/lib/add.spec.js
+++ src/lib/add.spec.js
@@ -5,15 +5,19 @@ describe('add function', () => {
     expect(add).toBeDefined()
   })
 
-  it('should accept an empty string and return a default value', () => {
+  it('should accept an empty string', () => {
     expect(add('')).toBe(0)
   })
 
-  it('should accept a single number and return it', () => {
+  it('should accept a single number', () => {
     expect(add('1')).toBe(1)
   })
 
-  it('should accept two numbers and return their sum', () => {
+  it('should accept two numbers', () => {
     expect(add('1,2')).toBe(3)
+  })
+
+  it('should accept an unknown amount of numbers', () => {
+    expect(add('1,2,3,4,5')).toBe(15)
   })
 })
