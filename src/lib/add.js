af0e6bdb2d6ca387b4f33db8ce33cf678c8397eb
Dev Wonderflow <58233289+alessio-libardi@users.noreply.github.com>
2021-11-28 20:25:42 +0100

    feat: support a string containing two numbers

--- src/lib/add.js
+++ src/lib/add.js
@@ -1,5 +1,10 @@
-function add(number) {
-  return number ? parseInt(number) : 0
+import { add as sum, map, pipe, reduce } from 'ramda'
+
+function add(numbers) {
+  const mapToNumber = map(Number)
+  const reduceToSum = reduce(sum, 0)
+
+  return pipe(mapToNumber, reduceToSum)(numbers.split(','))
 }
 
 export { add }
--- src/lib/add.spec.js
+++ src/lib/add.spec.js
@@ -12,4 +12,8 @@ describe('add function', () => {
   it('should accept a single number and return it', () => {
     expect(add('1')).toBe(1)
   })
+
+  it('should accept two numbers and return their sum', () => {
+    expect(add('1,2')).toBe(3)
+  })
 })
